418ab3633d995fa67a14e9fdadbc2670838e081b
molang404 <art104233@naver.com>
2026-08-14 17:10:52 +0900

    deleteNotice 작성(<- 관리자 전용, 진짜 삭제)

--- src/main/java/com/lineacademy/fridgemanagerspring/service/NoticeService.java
+++ src/main/java/com/lineacademy/fridgemanagerspring/service/NoticeService.java
@@ -36,4 +36,15 @@ public class NoticeService {
 
         return noticeRepository.save(notice);
     }
+
+    // 진짜 삭제하는 메서드
+    @Transactional
+    public void deleteNotice(Long noticeId) {
+        Notice notice = getNoticeById(noticeId);
+        if (notice != null) {
+            noticeRepository.delete(notice);
+        } else {
+            throw new IllegalArgumentException("NOT_FOUND_NOTICE");
+        }
+    }
 }
